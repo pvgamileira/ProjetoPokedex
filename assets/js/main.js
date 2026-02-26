bbd82ca82dc81b948d973f45b23bc361c46ab852
pvgamileira <pvgamileira@gmail.com>
2026-02-26 15:17:39 -0300

    Att projeto.

---
+++ assets/js/main.js
@@ -0,0 +1,38 @@
+const offset = 0
+const limit = 10
+
+const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
+
+function convertPokemonToLi(pokemon) {
+    return `
+            <li class="pokemon">
+                <span class="number">#001</span>
+                <span class="name">${pokemon.name}</span>
+
+                <div class="detail">
+                    <ol class="types">
+                        <li class="type">Grass</li>
+                        <li class="type">Poison</li>
+                    </ol>
+
+                    <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg"
+                        alt="${pokemon.name}" width="100">
+                </div>
+
+            </li>
+        `
+}
+
+fetch(url)
+    .then((response) => response.json())
+    .then((jsonBody) => jsonBody.results)
+
+    .then((pokemonList) => {
+        for (let i = 0; i < pokemonList.length ; i++) {
+            const pokemon = pokemonList[i];
+            console.log(convertPokemonToLi(pokemon))
+        }
+
+        
+    })
+    .catch((error) => { console.log(error) })
