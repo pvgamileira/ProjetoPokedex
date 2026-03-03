95e63d856c48a5b6e6f0ad50b65d35d4e78cb888
pvgamileira <pvgamileira@gmail.com>
2026-03-03 00:59:52 -0300

    Atualizando repo com arquivos da máquina-d-combate

---
+++ assets/js/main.js
@@ -0,0 +1,32 @@
+function convertPokemonTypesToLi(pokemontypes) {
+    return pokemontypes.map((typeSlot) => `<li class="type">${typeSlot.type.name}</li>`)
+}
+
+
+function convertPokemonToLi(pokemon) {
+    return `
+            <li class="pokemon">
+                <span class="number">${'#' + pokemon.order }</span>
+                <span class="name">${pokemon.name}</span>
+
+                <div class="detail">
+                    <ol class="types">
+                       ${convertPokemonTypesToLi(pokemon.types).join('')}
+                    </ol>
+
+                    <img src="${pokemon.sprites.other.dream_world.front_default}"
+                        alt="${pokemon.name}">
+                </div>
+
+            </li>
+        `
+}
+
+
+const pokemonList = document.getElementById('pokemonList')
+
+pokeAPI.getPokemons().then((pokemons = []) => {
+    const newHtml = pokemons.map(convertPokemonToLi).join('')
+    pokemonList.innerHTML = newHtml
+
+})
---
+++ assets/js/poke-api.js
@@ -0,0 +1,18 @@
+const pokeAPI = {}
+
+pokeAPI.getPokemonDetail = (pokemon) => {
+    return fetch(pokemon.url).then((response) => response.json())
+}
+
+pokeAPI.getPokemons = (offset = 0, limit = 5) => {
+    const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
+    
+    return fetch(url)
+        .then((response) => response.json())
+        .then((jsonBody) => jsonBody.results)
+        .then((pokemons) => pokemons.map(pokeAPI.getPokemonDetail))
+        .then((detailRequests) => Promise.all(detailRequests))
+        .then((pokemonsDetails) => pokemonsDetails)
+}
+
+
